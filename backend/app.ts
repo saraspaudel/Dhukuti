10914328c53e101bc30d8b3020cfa5d661152164
Saras Paudel <paudel.saras@gmail.com>
2021-06-14 12:21:08 -0500

    Register and login implemented

--- backend/app.ts
+++ backend/app.ts
@@ -3,23 +3,58 @@ import mysql from 'mysql';
 
 const app = express();
 
+app.use(express.json()); //Used to parse JSON bodies
+app.use(express.urlencoded()); //Parse URL-encoded bodies
+
 const port = 3000;
 
-app.get('/', (req, res) => {
-  var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '1234',
-    database: 'dhukuti'
-  });
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '1234',
+  database: 'dhukuti'
+});
 
-  connection.connect();
+connection.connect((error) => {
+  if (error) throw error;
+});
+
+app.post('/login', (req, res) => {
+  var email = req.body.email;
+  var password = req.body.password;
 
   connection.query('SELECT * FROM dhukuti.Users;', function (error, results, fields) {
     if (error) throw error;
-    console.log('The solution is: ', results);
-    res.send(results)
+    console.log('Users are : ', results)
+    let match = results.find(element => element.email === email);
+    
+    if (match == null) {
+      res.sendStatus(404);
+      console.log("Couldn't find user by that email address.")
+    } else if (match.password != password) {
+      res.sendStatus(404);
+      console.log("Wrong password")
+    }
+    else {
+      console.log("User found")
+      res.sendStatus(200);
+    }
+  });
+})
+
+app.post('/register', (req, res) => {
+  console.log('Got body: ', req.body);
+  res.sendStatus(200);
+
+  var firstName = req.body.FirstName;
+  var lastName = req.body.LastName;
+  var email = req.body.email;
+  var password = req.body.password;
+
+  connection.query(`INSERT INTO Users(FirstName, LastName, email, password)VALUES('${firstName}','${lastName}','${email}', '${password}');`, function (error, results, fields) {
+    if (error) throw error;
+    console.log('Result is: ', results);
   });
 })
 
