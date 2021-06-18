265980341f1c5081a40efb522be4cb5548c273ca
Saras Paudel <paudel.saras@gmail.com>
2021-06-18 14:27:45 -0500

    Finished login, getting ordering of loan with id, create loan and ordering

--- backend/app.ts
+++ backend/app.ts
@@ -21,31 +21,37 @@ connection.connect((error) => {
 });
 
 app.post('/login', (req, res) => {
+  console.log("Request is: ", req.body)
   var email = req.body.email;
   var password = req.body.password;
 
-  connection.query('SELECT * FROM dhukuti.Users;', function (error, results, fields) {
+  connection.query('SELECT * FROM Users WHERE email = ?', [email], function (error, results, fields) {
     if (error) throw error;
-    console.log('Users are : ', results)
-    let match = results.find(element => element.email === email);
+    console.log('Users are : ', results);
+
+    let user = results[0];
     
-    if (match == null) {
-      res.sendStatus(404);
-      console.log("Couldn't find user by that email address.")
-    } else if (match.password != password) {
-      res.sendStatus(404);
-      console.log("Wrong password")
+    if (user.length == 0) {
+      res.status(401).send("Couldn't find user by that email address.");
+      console.log("Email address not found.")
+    } else if (user.password != password) {
+      res.status(401).send("Username or password is incorrect.");
+      console.log("Email right, password wrong")
     }
     else {
       console.log("User found")
-      res.sendStatus(200);
+      res.status(200).send({
+        userId: user.userId,
+        FirstName: user.FirstName,
+        LastName: user.LastName,
+        email: user.email
+      });
     }
   });
 })
 
 app.post('/register', (req, res) => {
   console.log('Got body: ', req.body);
-
   var firstName = req.body.FirstName;
   var lastName = req.body.LastName;
   var email = req.body.email;
@@ -75,24 +81,50 @@ app.get('/loans', (req, res) => {
   });
 })
 
-app.post('/createLoan', (req, res) => {
+app.get('/loanorder', (req, res) => {
+  console.log("Called loan order get api");
+
+  var loanId = req.query.loanId;
+
+  let sql = `SELECT * FROM dhukuti.loanOrder WHERE loanId = ${loanId};`;
+
+  connection.query(sql, (error, results, fields) => {
+    console.log(results);
+    res.status(200).send(results);
+  })
+
+})
+
+app.post('/createLoan', async (req, res) => {
   var loanAmount = req.body.loanAmount;
   var totalParticipants = req.body.totalParticipants;
   var proposerUserId = req.body.proposerUserId;
   var loanFrequencyInDays = req.body.loanFrequencyInDays;
   var loanInterestRate = req.body.loanInterestRate;
+  var loanId;
 
   console.log("Creating new loan");
   let sql = `INSERT INTO Loans(loanAmount, totalParticipants, proposerUserId, loanFrequencyInDays, loanInterestRate)VALUES('${loanAmount}','${totalParticipants}','${proposerUserId}', '${loanFrequencyInDays}', '${loanInterestRate}');`;
   connection.query(sql, function (error, results, fields) {
     if (error) { res.sendStatus(500); throw error };
-    res.sendStatus(200);
-  });
+    loanId = results.insertId;
+    console.log("Loan created with id ", loanId);
 
-  for (let turn = 0; turn < totalParticipants; turn++) {
-    
-    
-  }
+    let increments = loanInterestRate / (totalParticipants/2);
+    let interestRate = loanInterestRate;
+
+    for (let turn = 1; turn != totalParticipants+1; turn++) {
+      console.log("Generating loan orders");
+      let sql = `INSERT INTO loanOrder (loanId, turn, interestRate) VALUES ('${loanId}', '${turn}', '${interestRate}');`
+      connection.query(sql, function (error, results, fields) {
+        if (error) { throw error };
+        console.log("Inserted loan order: ", results);
+      });
+      interestRate =  interestRate - increments;
+    }
+
+    res.status(201).send({loanId: loanId});
+  });
 
 })
 
