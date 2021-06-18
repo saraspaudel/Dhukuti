206474217dd23fa940220451f6d7ca741706bf76
Saras Paudel <paudel.saras@gmail.com>
2021-06-18 14:30:59 -0500

    Pick turn with interest rate hooked up to backend

--- frontend/lib/screens/pick_turn.dart
+++ frontend/lib/screens/pick_turn.dart
@@ -1,20 +1,28 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dhukuti/screens/marketplace.dart';
-import 'package:dhukuti/services/database.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:dhukuti/models/loan_order.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:number_display/number_display.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:async';
 
 class PickTurn extends StatefulWidget {
+  final String loanId;
+
+  PickTurn(this.loanId);
+
   @override
   _PickTurnState createState() => _PickTurnState();
 }
 
 class _PickTurnState extends State<PickTurn> {
-  String dropdownValue;
+  late String dropdownValue;
+  late Future<LoanOrderList> futureLoanOrder;
 
+  @override
+  void initState() {
+    super.initState();
+    futureLoanOrder = fetchLoans(widget.loanId);
+  }
+  
   @override
   Widget build(BuildContext context) {
     return new GestureDetector(
@@ -32,30 +40,43 @@ class _PickTurnState extends State<PickTurn> {
                   padding: const EdgeInsets.all(10.0),
                   child: Row(
                     children: [
-                      Expanded(
-                        child: DropdownButton<String>(
-                          hint: new Text("Pick your turn"),
-                          isExpanded: true,
-                          value: dropdownValue,
-                          icon: const Icon(Icons.arrow_downward),
-                          iconSize: 24,
-                          elevation: 16,
-                          underline: Container(
-                            height: 2,
-                            color: Colors.blue,
-                          ),
-                          onChanged: (String newValue) {
-                            setState(() {
-                              dropdownValue = newValue;
-                            });
+                      Center(
+                        child: FutureBuilder<LoanOrderList>(
+                          future: futureLoanOrder,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+
+                              return DropdownButton<String>(
+                                hint: new Text("Pick your turn"),
+                                isExpanded: false,
+                                icon: const Icon(Icons.arrow_downward),
+                                iconSize: 24,
+                                elevation: 16,
+                                underline: Container(
+                                  height: 2,
+                                  color: Colors.blue,
+                                ),
+                                onChanged: (String? newValue) => setState(() {
+                                  dropdownValue = newValue!;
+                                }),
+                                // items: snapshot.data.loans.toList(),
+                                items: snapshot.data!.loans.map<DropdownMenuItem<String>>((LoanOrder value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value.loanId.toString(),
+                                    child: Text("Turn " + value.turn.toString() + " and " + value.interestRate.toString() + "% Interest Rate "),
+                                  );
+                                }).toList(),
+                              );
+
+
+
+                            } else if (snapshot.hasError) {
+                              return Text("${snapshot.error}");
+                            }
+
+                            // By default, show a loading spinner.
+                            return CircularProgressIndicator();
                           },
-                          items: <String>['1st 5% Interest', '2nd 4.8 Interest', '3rd 4.6% Interest', '4th 4.4% Interest', '5th 4.2% Interest']
-                              .map<DropdownMenuItem<String>>((String value) {
-                            return DropdownMenuItem<String>(
-                              value: value,
-                              child: Text(value),
-                            );
-                          }).toList(),
                         ),
                       )
                     ],
@@ -80,5 +101,20 @@ class _PickTurnState extends State<PickTurn> {
                 ),
               ),
             ])));
+  }
+}
+
+Future<LoanOrderList> fetchLoans(String loanId) async {
+  final response = await http.get(Uri.parse('http://10.0.2.2:3000/loanOrder?loanId=' + loanId));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    print("Ordering of Loan ID: " + loanId.toString() + response.body.toString());
+    return LoanOrderList.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load loan order');
   }
 }
