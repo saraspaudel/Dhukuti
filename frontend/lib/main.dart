907f31f239ef779682d4f82fc4687abfedc9f933
Saras Paudel <paudel.saras@gmail.com>
2021-06-18 14:28:46 -0500

    Remove firebase auth

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,13 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:dhukuti/services/auth.dart';
 import 'package:dhukuti/screens/login.dart';
-import 'package:dhukuti/screens/home.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer';
-
-// Import the firebase_core plugin
-import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(App());
@@ -18,33 +10,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      home: FutureBuilder(
-        // Initialize FlutterFire:
-        future: Firebase.initializeApp(),
-        builder: (context, snapshot) {
-          // Check for errors
-          if (snapshot.hasError) {
-            log(snapshot.error.toString());
-            return const Scaffold(
-              body: Center(
-                child: Text("Error"),
-              ),
-            );
-          }
-
-          // Once complete, show your application
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Root();
-          }
-
-          // Otherwise, show something whilst waiting for initialization to complete
-          return const Scaffold(
-            body: Center(
-              child: Text("Loading..."),
-            ),
-          );
-        },
-      ),
+      home: Login()
     );
   }
 }
@@ -55,34 +21,9 @@ class Root extends StatefulWidget {
 }
 
 class _RootState extends State<Root> {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-      stream: Auth(auth: _auth).usr,
-      builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
-        if (snapshot.connectionState == ConnectionState.active){
-          if(snapshot.data?.uid == null){
-            return Login(
-              auth: _auth,
-              firestore: _firestore,
-            );
-          } else {
-            return Home(
-              auth: _auth,
-              firestore: _firestore,
-            );
-          }
-        } else {
-          return const Scaffold(
-            body: Center(
-              child: Text("Loading..."),
-            ),
-          );
-        }
-      },
-    );
+    return Login();
   }
 }
