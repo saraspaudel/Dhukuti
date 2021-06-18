b5a249b90ab0e2eed74151ea83ec76cac3b980b8
Saras Paudel <paudel.saras@gmail.com>
2021-06-18 14:30:37 -0500

    Linked marketplace to backend

--- frontend/lib/screens/marketplace.dart
+++ frontend/lib/screens/marketplace.dart
@@ -1,24 +1,24 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dhukuti/models/loanpool.dart';
-import 'package:dhukuti/services/database.dart';
 import 'package:dhukuti/widgets/loan_pool_card.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class MarketPlace extends StatefulWidget {
-  final FirebaseAuth auth;
-  final FirebaseFirestore firestore;
-
-  const MarketPlace({
-    Key key,
-    @required this.auth,
-    @required this.firestore,
-  }) : super(key: key);
   @override
   _MarketPlaceState createState() => _MarketPlaceState();
 }
 
 class _MarketPlaceState extends State<MarketPlace> {
+  late Future<LoanPoolList> futureLoans;
+
+  @override
+  void initState() {
+    super.initState();
+    futureLoans = fetchLoans();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,37 +28,53 @@ class _MarketPlaceState extends State<MarketPlace> {
             height: 10,
           ),
           Expanded(
-            child: StreamBuilder(
-              stream: Database(firestore: FirebaseFirestore.instance)
-                  .streamTodos(uid: FirebaseAuth.instance.currentUser.uid),
-              builder: (BuildContext context,
-                  AsyncSnapshot<List<LoanPoolModel>> snapshot) {
-                if (snapshot.connectionState == ConnectionState.active) {
-                  if (!snapshot.hasData) {
-                    return const Center(
-                      child: Text("No Loan pools available"),
-                    );
+            child: Center(
+              child: FutureBuilder<LoanPoolList>(
+                future: futureLoans,
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                  return Text("${snapshot.error}");
                   }
-                  return ListView.builder(
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (_, index) {
-                      return LoanPoolCard(
-                        firestore: widget.firestore,
-                        uid: FirebaseAuth.instance.currentUser.uid,
-                        loanPool: snapshot.data[index],
-                      );
-                    },
-                  );
-                } else {
-                  return const Center(
-                    child: Text("loading..."),
-                  );
-                }
-              },
+                  else if (snapshot.data!.isEmpty()) {
+                    print("No Loan pools available.");
+                    return const Text("No Loan pools available");
+                  }
+                  else if (snapshot.hasData) {
+                    return ListView.builder(
+                      itemCount: snapshot.data!.loans.length,
+                      itemBuilder: (_, index) {
+                        return LoanPoolCard(
+                          loanPool: snapshot.data!.loans[index],
+                        );
+                      },
+                    );
+                    // return Text(snapshot.data.loans[0].loanAmount.toString());
+                  }
+
+                  // By default, show a loading spinner.
+                  return CircularProgressIndicator();
+                },
+              ),
             ),
           ),
         ],
       ),
     );
+  }
+}
+
+
+Future<LoanPoolList> fetchLoans() async {
+  final response = await http.get(Uri.parse('http://10.0.2.2:3000/loans'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    print("Loans: " + response.body.toString());
+    return LoanPoolList.fromJson(jsonDecode(response.body));
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load loans');
   }
 }
